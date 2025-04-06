ff0e2b8f2a420f5be52ca4e93545d873db283c7f
QS <1942598111@qq.com>
2025-04-06 13:15:32 +0800

    Update MY_Tools_2022.py
    detele file

--- MY_menu/plug-ins/MY_Tools_2022.py
+++ MY_menu/plug-ins/MY_Tools_2022.py
@@ -4,38 +4,3 @@
 # .@Aurhor..:MingYu
 # .@Contact.:1942598111@qq.com
 """
-launch :
-        import MY_Tools_2020 as kt_FileName
-        import importlib.reload as relaad
-        reload(FileName)
-        FileName.main()
-"""
-import maya.cmds as cmds
-# _*_ coding: utf-8 _*_
-# .@FileName:MY_Tools_2023
-# .....:2022-09-12 : 16 :42
-# .@Aurhor..:MingYu
-# .@Contact.:1942598111@qq.com
-"""
-launch :
-        import MY_Tools_2023 as kt_FileName
-        reload(FileName)
-        FileName.main()
-"""
-import maya.cmds as cmds
-import MY_MenuUI_2020
-import importlib
-#from importlib import reload
-reload(MY_MenuUI_2020)
-
-def load_MY_tools():
-    MY_MenuUI_2020.MY_Menu_UI()
-
-def unLoad_MY_tools():
-    MY_MenuUI_2020.delete_MY_Menu_UI()
-
-def initializePlugin(mobject):
-    load_MY_tools()
-
-def uninitializePlugin(mobject):
-    load_MY_tools()
